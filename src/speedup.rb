fbba25236d7454ba49890e50c6b7ebc153c8bcc3
Thomas Thomassen <thomas@thomthom.net>
2014-10-08 21:05:39 +0200

    Moving menu generation code from SUbD into SpeedUp.

--- src/speedup.rb
+++ src/speedup.rb
@@ -1,4 +1,3 @@
-gem "ruby-prof"
 require "ruby-prof"
 
 require "benchmark"
@@ -198,6 +197,39 @@ module SpeedUp
     window.set_file(report)
     window.show
     @windows << window
+  end
+
+
+  # @param [Sketchup::Menu] menu
+  # @param [Class, Module] namespace
+  #
+  # @return [Nil]
+  def self.build_menus(menu, namespace)
+    profile_klasses = self.discover_profile_classes(namespace)
+    profile_klasses.each { |profile_klass|
+      klass_menu = menu.add_submenu(profile_klass.test_name)
+      klass_menu.add_item("Benchmark") { profile_klass.benchmark }
+      klass_menu.add_separator
+      profile_instance = profile_klass.new
+      profile_klass.each_test_with_name { |method, name|
+        klass_menu.add_item(name) { profile_klass.run(method) }
+      }
+    }
+    nil
+  end
+
+
+  # @param [Class, Module] namespace
+  #
+  # @return [SpeedUp::ProfileTest]
+  def self.discover_profile_classes(namespace)
+    consts = namespace.constants.map { |const|
+      namespace.const_get(const)
+    }
+    klasses = consts.select { |object| object.class == Class }
+    klasses.select { |klass|
+      klass.ancestors.include?(SpeedUp::ProfileTest)
+    }
   end
 
 end # module
